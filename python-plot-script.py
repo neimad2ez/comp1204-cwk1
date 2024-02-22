d26995ea78a0075303f85dbc61e0426f62e09061
dt2g23 <dt2g23@soton.ac.uk>
2024-02-22 17:07:48 +0000

    Ran the file before setting username and email, conflict didn't work, now works

---
+++ python-plot-script.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import glob
+import math
+user_key = 28347
+
+def plot_all_csv_pressure():
+    path = os.getcwd()
+    csv_files = glob.glob(os.path.join(path, '*.csv'))
+    
+    for f in csv_files:
+        storm = pd.read_csv(f)
+        storm['Pressure'].plot()
+        plt.show()
