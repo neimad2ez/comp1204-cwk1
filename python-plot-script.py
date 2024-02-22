5c81aeaa77fce3f3e488da99c9df96fb2951371a
Damien <dt2g23@soton.ac.uk>
2024-02-22 17:08:53 +0000

    Added new function to the python-plot-script from python-addon

---
+++ python-plot-script.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+import glob
+import matplotlib.pyplot as plt
+user_key= 23944
+
+def plot_all_csv_pressure():
+    path = os.getcwd()
+    csv_files = glob.glob(os.path.join(path, '*.csv'))
+    
+    fr f in csv_files:
+        storm = pd.read_csv(f)
+        storm['Pressure'].plot()
+        plt.show()
+		
+def plot_all_csv_intensity():
+    path = os.getcwd()
+    csv_files = glob.glob(os.path.join(path, '*.csv'))
+    
+    for f in csv_files:
+        storm = pd.read_csv(f)
+        storm['Intensity'].plot()
+        plt.show()
