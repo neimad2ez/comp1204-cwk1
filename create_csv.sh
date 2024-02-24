2119e71a25edff3c59a1d3d63e1b4230ca9238de
Damien <dt2g23@soton.ac.uk>
2024-02-24 17:31:20 +0000

    sh: line 1: q: command not found
    This reverts commit 9bccbdda8323d0dbb0c9d00b1056f5723968679e.

--- create_csv.sh
+++ create_csv.sh
@@ -3,17 +3,11 @@ csv_input_path=$1
 tmp_csv_output=$2
 
 echo "Timestamp,Latitude,Longitude,MinSeaLevelPressure,MaxIntensity" > $tmp_csv_output
-dtg="$(grep -R "<dtg>" $csv_input_path | sed 's/.*<dtg>//g' | sed 's/[</dtg>]//g')"
-grep -R "<lat>" $csv_input_path| sed 's/.*<lat>//g' | sed 's/[</lat>]//g' > lat.csv
-grep -R "<lon>" $csv_input_path | sed 's/.*<lon>//g' | sed 's/[</lon>]//g' > lon.csv
-grep -R "<minSeaLevelPres>" $csv_input_path | sed 's/.*<minSeaLevelPres>//g' | sed 's/[</minSeaLevelPres>]//g' > minSeaLevelPres.csv
-grep -R "<intensity>" $csv_input_path | sed 's/.*<intensity>//g' | sed 's/[</intensity>]//g' > intensity.csv
-lat="$(sed "s/$/ N/" lat.csv)"
-lon="$(sed "s/$/ W/" lon.csv)"
-minSeaLevelPres="$(sed "s/$/ mb/" minSeaLevelPres.csv)"
-maxIntensity="$(sed "s/$/ knots/" intensity.csv)"
+dtg="$(grep -R "<dtg>" $csv_input_path| sed 's/.*<dtg>//g' | sed 's/[</dtg>]//g')" >> $tmp_csv_output
+lat="$(cat $csv_input_path | grep "<lat>" | sed 's/.*<lat>//g' | sed 's/[</lat>]//g')"
+lon="$(cat $csv_input_path | grep "<lon>" | sed 's/.*<lon>//g' | sed 's/[</lon>]//g')"
+minSeaLevelPres="$(cat $csv_input_path | grep "<minSeaLevelPres>" | sed 's/.*<minSeaLevelPres>//g' | sed 's/[</minSeaLevelPres>]//g')"
+maxIntensity="$(cat $csv_input_path | grep "<intensity>" | sed 's/.*<intensity>//g' | sed 's/[</intensity>]//g')"
 paste -d',' <(echo "$dtg") <(echo "$lat") <(echo "$lon") <(echo "$minSeaLevelPres") <(echo "$maxIntensity") >> $tmp_csv_output
-rm lat.csv
-rm lon.csv
-rm minSeaLevelPres.csv
-rm intensity.csv
+#NAME=${dtg}_${lat}_${lon}_${minSeaLevelPres}_${maxIntensity}
+#echo $NAME >> $tmp_csv_output
